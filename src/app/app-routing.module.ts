c23f74b10df50a411f61486729decccf042e3c68
Samarth-Salian <samarth.salain@i-exceed.com>
2021-03-15 10:24:02 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,54 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LocationDetailsComponent } from './location/location-details/location-details.component';
+import { LocationListComponent } from './location/location-list/location-list.component';
+import { MeetingDetailsComponent } from './meeting/meeting-details/meeting-details.component';
+import { MyMeetingsComponent } from './meeting/my-meetings/my-meetings.component';
+import { RoomDetailsComponent } from './room/room-details/room-details.component';
+import { RoomListComponent } from './room/room-list/room-list.component';
+import { RoomSearchComponent } from './room/room-search/room-search.component';
+import { SigninComponent } from './signin/signin.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    {
+      path:"",
+      component: SigninComponent
+    },
+    {
+      path:"my-meetings",
+      component: MyMeetingsComponent
+    },
+    {
+      path:"room-search",
+      component: RoomSearchComponent
+    },
+    {
+      path:"room-list",
+      component: RoomListComponent
+    },
+    {
+      path:"meeting-details",
+      component: MeetingDetailsComponent
+    },
+    {
+      path:"location-list",
+      component: LocationListComponent
+    },    
+    {
+      path:"location-details",
+      component: LocationDetailsComponent
+    }, 
+    {
+      path:"room-details",
+      component: RoomDetailsComponent
+    },
+    {
+      path:"user-list",
+      component: UserListComponent
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { CalenderComponent } from './calender/calender.component';
+import { MeetingListComponent } from './meeting/meeting-list/meeting-list.component';
+import { RoomListComponent } from './room/room-list/room-list.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { RoomSearchComponent } from './room/room-search/room-search.component';
+import { MyMeetingsComponent } from './meeting/my-meetings/my-meetings.component';
+import { LocationListComponent } from './location/location-list/location-list.component';
+import { LocationDetailsComponent } from './location/location-details/location-details.component';
+import { MeetingDetailsComponent } from './meeting/meeting-details/meeting-details.component';
+import { RoomDetailsComponent } from './room/room-details/room-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    CalenderComponent,
+    MeetingListComponent,
+    RoomListComponent,
+    UserListComponent,
+    RoomSearchComponent,
+    MyMeetingsComponent,
+    LocationListComponent,
+    LocationDetailsComponent,
+    MeetingDetailsComponent,
+    RoomDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]

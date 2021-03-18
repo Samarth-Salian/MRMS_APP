20b3c0c18c502ad8ef160eca6b04ea243da7e18f
Pratik Mandal <pratikmandal3001@gmail.com>
2021-03-18 12:43:28 +0530

    Meeting App signin and roomlist

--- src/app/room/room-list/room-list.component.ts
+++ src/app/room/room-list/room-list.component.ts
@@ -6,9 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-list.component.css']
 })
 export class RoomListComponent implements OnInit {
-
+  test = [
+    {
+        "_id": "6051bbae78b0bb34a04fd90b",
+        "name": "Room D",
+        "seats": 3,
+        "phoneNo": "+919012788889",
+        "floor": "2",
+        "buildingId": "7878",
+        "voipAvailable": true,
+        "videoAvailable": true,
+        "lanAvailable": true,
+        "whiteBoardAvailable": true,
+        "createdAt": "2021-03-17T08:19:58.233Z",
+        "updatedAt": "2021-03-17T08:19:58.233Z",
+        "__v": 0
+    },
+    {
+        "_id": "6051bbdc78b0bb34a04fd90c",
+        "name": "Room A",
+        "seats": 12,
+        "phoneNo": "+919012781224",
+        "floor": "1",
+        "buildingId": "7878",
+        "voipAvailable": true,
+        "videoAvailable": false,
+        "lanAvailable": true,
+        "whiteBoardAvailable": true,
+        "createdAt": "2021-03-17T08:20:44.712Z",
+        "updatedAt": "2021-03-17T08:20:44.712Z",
+        "__v": 0
+    }
+]
   constructor() { }
-
+  isShowDiv = true;
+  toggleDisplayDiv(){
+    this.isShowDiv = !this.isShowDiv;
+  }
   ngOnInit(): void {
   }
 

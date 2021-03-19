b213b42dc78b8f143664fedc464b9ef1fe34500d
NikitaNavajeevan <nikita.n@i-exceed.com>
2021-03-19 10:57:47 +0530

    locationList UI implemented and MeetingList Time

--- src/app/meeting/meeting-list/meeting-list.component.ts
+++ src/app/meeting/meeting-list/meeting-list.component.ts
@@ -10,16 +10,44 @@ import { Observable } from 'rxjs';
 })
 export class MeetingListComponent implements OnInit {
 
-  tempjson: any;
+  meetings: any;
   constructor(public http: HttpClient) {
     this.getjson().subscribe(data => {
-      this.tempjson = data;
-      })
+      this.meetings = data;
+    })
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
   public getjson(): Observable<any> {
     return this.http.get("assets/meetingList.json").pipe()
+  }
+  convertTime(timeVal: any) {
+    let convertTime: any = (timeVal / 4).toString().split(".");
+    switch (convertTime[1]) {
+      case '25':
+        convertTime[1] = ".15";
+        break;
+      case "5":
+        convertTime[1] = ".30";
+        break;
+      case '75':
+        convertTime[1] = ".45";
+        break;
+      default:
+        convertTime[1] = ".00";
+        break;
+    }
+    if (parseInt(convertTime[0]) > 12) {   //greater than 12hr
+      convertTime[1] = convertTime[1] + " PM";
+      convertTime[0] = parseInt(convertTime[0]) - 12;
+    } else if (parseInt(convertTime[0]) < 1) {   //between 12 to 1 hr - morning
+      convertTime[0] = "12";
+      convertTime[1] = convertTime[1] + " AM";
+    } else {    // less than 12 hr
+      convertTime[1] = convertTime[1] + " AM";
+    }
+
+    return convertTime.join('');
   }
 }
 

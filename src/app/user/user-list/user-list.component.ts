a047d0fec9b1d16fc93752469d0ed78ea65c44fd
MVManjunath <manjunath.mv@i-exceed.com>
2021-03-19 19:19:40 +0530

    userlist and myMeetings updated

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { UserServices } from '../../services/user.service'
+
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
@@ -8,16 +10,27 @@ import { Observable } from 'rxjs';
 })
 export class UserListComponent implements OnInit {
 users:any;
-constructor(public http: HttpClient) {
+constructor(public http: HttpClient, public userService: UserServices) {
   this.getjson().subscribe(data => {
     this.users = data;
   })
 }
 
+adminMsg: String = "This  user updated to Admin role";
+userMsg: String = "this user updated to User role";
+
+
   ngOnInit(): void {
   }
   public getjson(): Observable<any> {
     return this.http.get("assets/userList.json").pipe()
+  }
+  
+  getAdminMsg() {
+    this.userService.showSuccess(this.adminMsg);
+  }
+  getUserMsg() {
+    this.userService.showSuccess(this.userMsg);
   }
 
 }

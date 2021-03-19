87d73632e0f51c482ecdaeb1a00787e858479afe
bala453a1 <amalakanti.balakrishna@i-exceed.com>
2021-03-19 23:00:25 +0530

    CreateMeeting_issue_fix

--- src/app/room/room-search/room-search.component.ts
+++ src/app/room/room-search/room-search.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ThemePalette } from '@angular/material/core';
 import { $ } from 'protractor';
+import { DatePipe } from '@angular/common';
 import { AppComponent } from '../../app.component';
 
 @Component({
@@ -13,7 +14,7 @@ export class RoomSearchComponent implements OnInit {
   color: ThemePalette = 'primary';
   selected: any;
   locations = ['Select', 'Building A', 'Building B', 'Building C'];
-  constructor(private titleChange: AppComponent, private router: Router) {
+  constructor(private titleChange: AppComponent, private router: Router, public datepipe: DatePipe) {
     this.titleChange.setTitle();
   }
 
@@ -25,8 +26,8 @@ export class RoomSearchComponent implements OnInit {
   public fnNavigateToRoomList(params: any): any {
     let roomDetailsObj: any = {};
     roomDetailsObj.location = document.getElementById("buldingnames");
-    roomDetailsObj.location = roomDetailsObj.location.selectedOptions[0].innerText;
-    roomDetailsObj.date = params.date;
+    roomDetailsObj.location = roomDetailsObj.location.selectedOptions.length !== 0 ? roomDetailsObj.location.selectedOptions[0].innerText : "";
+    roomDetailsObj.date = this.datepipe.transform(params.date, 'yyyy-MM-dd');
     roomDetailsObj.fromTime = params.fromSlot;
     roomDetailsObj.toTime = params.toSlot;
     roomDetailsObj.attendees = params.attendee;

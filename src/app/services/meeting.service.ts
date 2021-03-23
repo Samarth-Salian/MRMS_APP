2324e44961018e2aaf12d84778e992f51e78e5ed
Aishwarya Munde <aishwarya.munde@i-exceed.com>
2021-03-23 15:21:16 +0530

    Toastr removed

--- src/app/services/meeting.service.ts
+++ src/app/services/meeting.service.ts
@@ -1,17 +1,10 @@
 import { Injectable } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MeetingService {
 
-  constructor(private toastr: ToastrService) { }
+  constructor() { }
 
-  showSuccess(message: any) {
-    this.toastr.success(message)
-  }
-  showError(message: any, title: any) {
-    this.toastr.error(message, title)
-  }
 }

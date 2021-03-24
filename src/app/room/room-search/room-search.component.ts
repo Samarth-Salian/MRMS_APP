14a5a63db766ba136c2b604fe3fd3d8f1700b287
Samarth-Salian <samarth.salain@i-exceed.com>
2021-03-24 19:43:20 +0530

    fixed missing bracket issue

--- src/app/room/room-search/room-search.component.ts
+++ src/app/room/room-search/room-search.component.ts
@@ -54,7 +54,8 @@ export class RoomSearchComponent implements OnInit {
     } else {
       this.router.navigateByUrl('/room-list', { state: { data: this.roomSearch } });
     }
-
+  }
+  
   onCalendarChange(pthis: any) {
     this.roomSearch.date = pthis.targetElement.value;
   }

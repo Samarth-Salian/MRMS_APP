6946c3e5514d7f6cdee440b1c6c25a608b59e9e8
Aishwarya Munde <aishwarya.munde@i-exceed.com>
2021-03-24 18:15:00 +0530

    ESlint issue fixing

--- src/app/meeting/meeting-details/meeting-details.component.ts
+++ src/app/meeting/meeting-details/meeting-details.component.ts
@@ -1,45 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { AppComponent } from '../../app.component';
 import { Room } from '../../models/room';
 import { Meeting } from '../../models/meeting';
-import { ActivatedRoute } from '@angular/router';
+
 import { SnackbarService } from '../../services/snackbar.service';
 
 @Component({
   selector: 'app-meeting-details',
   templateUrl: './meeting-details.component.html',
-  styleUrls: ['./meeting-details.component.css']
+  styleUrls: ['./meeting-details.component.css'],
 })
 export class MeetingDetailsComponent implements OnInit {
-  meetingSchedule: string = "";
+  meetingSchedule: string = '';
+
   meeting: Meeting;
+
   selectedMeeting: Meeting;
+
   selectedRoom: Room;
-  submitMsg: string = "Meeting details submitted successfully";
-  constructor(public snackBar: SnackbarService, private activatedRoute: ActivatedRoute, private titleChange: AppComponent, private router: Router) {
-    this.titleChange.title = this.activatedRoute.snapshot.data['title'];
+
+  submitMsg: string = 'Meeting details submitted successfully';
+
+  constructor(public snackBar: SnackbarService, private activatedRoute: ActivatedRoute,
+    private titleChange: AppComponent, private router: Router) {
+    this.titleChange.title = this.activatedRoute.snapshot.data.title;
     this.titleChange.setTitle(this.titleChange.title);
     this.meeting = new Meeting();
     this.selectedMeeting = new Meeting();
     this.selectedRoom = new Room();
-    console.log(history.state);
-    if (typeof (history.state.data) !== "undefined") {
-      if (history.state.flow === "createMeeting") {
+
+    if (typeof (history.state.data) !== 'undefined') {
+      if (history.state.flow === 'createMeeting') {
         this.selectedRoom = history.state.data;
-        if (typeof (history.state.data.roomCreationDetails._date) !== "undefined") {
+        if (typeof (history.state.data.roomCreationDetails._date) !== 'undefined') {
           this.meetingSchedule = history.state.data.roomCreationDetails._date + " " + history.state.data.roomCreationDetails._fromTime + " " + history.state.data.roomCreationDetails._toTime;
         }
         this.meeting.location = history.state.data.roomCreationDetails._location;
         this.meeting.room = this.selectedRoom.name;
         this.meeting.fromTime = this.selectedRoom.fromTime;
         this.meeting.toTime = this.selectedRoom.toTime;
-        this.meeting.date = this.selectedRoom.date
+        this.meeting.date = this.selectedRoom.date;
         this.meeting.seats = history.state.data.roomCreationDetails._seats;
-      } else if (history.state.flow === "editMeeting") {
+      } else if (history.state.flow === 'editMeeting') {
         this.selectedMeeting = history.state.data;
-        if (typeof (this.selectedMeeting.date) !== "undefined") {
-          this.meetingSchedule = this.selectedMeeting.date + " " + this.selectedMeeting.fromTime + " " + this.selectedMeeting.toTime;
+        if (typeof (this.selectedMeeting.date) !== 'undefined') {
+          this.meetingSchedule = this.selectedMeeting.date + ' ' + this.selectedMeeting.fromTime + ' ' + this.selectedMeeting.toTime;
         }
         this.meeting.name = this.selectedMeeting.name;
         this.meeting.location = this.selectedMeeting.location;
@@ -47,23 +53,25 @@ export class MeetingDetailsComponent implements OnInit {
         this.meeting.seats = history.state.data.attendees;
         this.meeting.fromTime = this.selectedMeeting.fromTime;
         this.meeting.toTime = this.selectedMeeting.toTime;
-        this.meeting.date = this.selectedMeeting.date
+        this.meeting.date = this.selectedMeeting.date;
       } else {
-        //TODO handle exception
+        //  TODO handle exception
       }
     } else {
-      //TODO handle undefined
+      //  TODO handle undefined
     }
   }
+
   ngOnInit(): void {
 
   }
+
   getSubmitMsg() {
     if (this.meeting.seats <= 0) {
       this.snackBar.openSnackBar('Seats should be more than 0', '');
     } else {
-    this.snackBar.openSnackBar(this.submitMsg, '');
-    this.router.navigateByUrl('/my-meetings', { state: { data: this.meeting } });
+      this.snackBar.openSnackBar(this.submitMsg, '');
+      this.router.navigateByUrl('/my-meetings', { state: { data: this.meeting } });
     }
   }
 }
--- src/app/models/room.ts
+++ src/app/models/room.ts
@@ -1,123 +1,132 @@
 export class Room {
-    private _name: string ="";
-    private _location: string = "";
-    private _date: string = "";
-    private _fromTime: string = "";
-    private _toTime: string = "";
+    private _name: string = '';
+
+    private _location: string = '';
+
+    private _date: string = '';
+
+    private _fromTime: string = '';
+
+    private _toTime: string = '';
+
     private _seats: number = 0;
-    private _phoneNo : string = "";
+
+    private _phoneNo: string = '';
+
     private _floor : number = -99;
-    private _buildingId : string ="";
+
+    private _buildingId: string = '';
+
     private _voipAvailable: boolean = false;
+
     private _videoAvailable: boolean = false;
+
     private _lanAvailable: boolean = false;
+
     private _whiteBoardAvailable: boolean = false;
-    
-  
-    constructor() { }
 
     public get name(): string {
-        return this._name;
+      return this._name;
     }
 
     public set name(name: string) {
-        this._name = name;
+      this._name = name;
     }
 
     public get location(): string {
-        return this._location;
+      return this._location;
     }
 
     public set location(location: string) {
-        this._location = location;
+      this._location = location;
     }
 
     public get date(): string {
-        return this._date;
+      return this._date;
     }
 
     public set date(date: string) {
-        this._date = date;
+      this._date = date;
     }
 
     public get fromTime(): string {
-        return this._fromTime;
+      return this._fromTime;
     }
 
     public set fromTime(fromTime: string) {
-        this._fromTime = fromTime;
+      this._fromTime = fromTime;
     }
 
     public get toTime(): string {
-        return this._toTime;
+      return this._toTime;
     }
 
     public set toTime(toTime: string) {
-        this._toTime = toTime;
+      this._toTime = toTime;
     }
 
     public get seats(): number {
-        return this._seats;
+      return this._seats;
     }
 
     public set seats(seats: number) {
-        this._seats = seats;
+      this._seats = seats;
     }
 
     public get phoneNo(): string {
-        return this._phoneNo;
+      return this._phoneNo;
     }
 
     public set phoneNo(phoneNo: string) {
-        this._phoneNo = phoneNo;
+      this._phoneNo = phoneNo;
     }
-    
+
     public get floor(): number {
-        return this._floor;
+      return this._floor;
     }
 
     public set floor(floor: number) {
-        this._floor = floor;
+      this._floor = floor;
     }
 
     public get buildingId(): string {
-        return this._buildingId;
+      return this._buildingId;
     }
 
     public set buildingId(buildingId: string) {
-        this._buildingId = buildingId;
+      this._buildingId = buildingId;
     }
 
     public get voipAvailable(): boolean {
-        return this._voipAvailable;
+      return this._voipAvailable;
     }
 
     public set voipAvailable(voipAvailable: boolean) {
-        this._voipAvailable = voipAvailable;
+      this._voipAvailable = voipAvailable;
     }
 
     public get videoAvailable(): boolean {
-        return this._videoAvailable;
+      return this._videoAvailable;
     }
 
     public set videoAvailable(videoAvailable: boolean) {
-        this._videoAvailable = videoAvailable;
+      this._videoAvailable = videoAvailable;
     }
 
     public get lanAvailable(): boolean {
-        return this._lanAvailable;
+      return this._lanAvailable;
     }
 
     public set lanAvailable(lanAvailable: boolean) {
-        this._lanAvailable = lanAvailable;
+      this._lanAvailable = lanAvailable;
     }
 
     public get whiteBoardAvailable(): boolean {
-        return this._whiteBoardAvailable;
+      return this._whiteBoardAvailable;
     }
 
     public set whiteBoardAvailable(whiteBoardAvailable: boolean) {
-        this._whiteBoardAvailable = whiteBoardAvailable;
-    }   
+      this._whiteBoardAvailable = whiteBoardAvailable;
+    }
 
 }

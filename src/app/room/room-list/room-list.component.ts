2a482a4a431e34e5e3b13ff8bbb47b0fb21d5b6d
MVManjunath <manjunath.mv@i-exceed.com>
2021-04-09 18:37:56 +0530

    swipe room list updated

--- src/app/room/room-list/room-list.component.ts
+++ src/app/room/room-list/room-list.component.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { Room } from '../../models/room';
 import { AppComponent } from '../../app.component';
-import { SnackbarService } from 'src/app/services/snackbar.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { RoomSearchComponent } from '../room-search/room-search.component';
 
 @Component({
@@ -14,6 +14,9 @@ import { RoomSearchComponent } from '../room-search/room-search.component';
   styleUrls: ['./room-list.component.css'],
 })
 export class RoomListComponent {
+  scrollElement: any;
+  deletedRecord: any;
+  deletedRow: number = 0;
   roomDetails: Room = history.state.data;
 
   rooms: Room[] = [];
@@ -21,7 +24,7 @@ export class RoomListComponent {
   conditionalFabIcon: boolean;
 
   constructor(private zone: NgZone, private titleChange: AppComponent, private router: Router,
-    private activatedRoute: ActivatedRoute, public http: HttpClient, private dialog: MatDialog, private snackBar: SnackbarService) {
+    private activatedRoute: ActivatedRoute, public http: HttpClient, private dialog: MatDialog, public snackBar: MatSnackBar) {
     this.titleChange.title = this.activatedRoute.snapshot.data.title;
     this.titleChange.setTitle(this.titleChange.title);
     this.conditionalFabIcon = this.titleChange.showFabIcon;
@@ -29,8 +32,9 @@ export class RoomListComponent {
     setTimeout(() => { this.fnInitializeSwipe() }, 500);
 
   }
-  public fnInitializeSwipe = function () {
-    fnSwipeList();
+  public fnInitializeSwipe = () => {
+    this.fnSwipeList();
+    this.restrictDelete();
   }
   subscribedRoomList() {
     this.getjson().subscribe(data => {
@@ -42,10 +46,27 @@ export class RoomListComponent {
     return this.http.get('assets/roomList.json').pipe();
   }
 
-  public fnDelete(i: any) {
-    console.log(i);
-    document.getElementsByClassName('listContainer')[i].remove();
-    this.snackBar.openSnackBar('Deleted Successfully', 'Undo');
+  public fnDelete(event: any) {
+    this.deletedRecord = event.target.closest('.listContainer');
+    this.deletedRow = parseInt(event.target.closest('.swipe-box').getAttribute('rowno'));
+    event.target.closest('.listContainer').remove();
+    let snackBarRef = this.snackBar.open('Deleted Successfully', 'Undo', {
+      duration: 2000,
+    });
+    snackBarRef.onAction().subscribe(() => {
+      console.log(this.deletedRow);
+      console.log(this.deletedRecord);
+      let currentRecord: any;
+      let swipeList: any = document.getElementsByClassName('swipe-box');
+      if ((this.deletedRow - 1) === parseInt(swipeList[swipeList.length - 1].getAttribute('rowno'))) {
+        currentRecord = document.getElementById('swipeBoxId_' + (this.deletedRow - 1));
+        currentRecord.parentElement.after(this.deletedRecord);
+      } else {
+        currentRecord = document.getElementById('swipeBoxId_' + (this.deletedRow + 1));
+        currentRecord.parentElement.before(this.deletedRecord);
+      }
+      this.fnSwipeList();
+    });
 
   }
 
@@ -74,27 +95,38 @@ export class RoomListComponent {
       }
     });
   }
-}
-function fnSwipeList() {
-  const swipeBoxes = document.querySelectorAll('.swipe-box');
-  swipeBoxes.forEach(swipeBox => {
-    const scroller = swipeBox.querySelector('.swipe-box__scroller');
-    if (scroller) {
-      scroller.scrollLeft += scroller.scrollWidth / 3;
+
+  public restrictDelete() {
+    let initialCard: any = document.getElementsByClassName('observe-item');
+    if (document.getElementsByClassName('listContainer ').length === 1) {
+      initialCard[0].style.display = 'none';
+    } else {
+      initialCard[0].style.display = 'block';
     }
-    fnDetectSwipe(swipeBox);
-  });
-}
+  }
 
-function fnDetectSwipe(swipeBoxObj: any) {
-  let touchstartX = 0;
-  let touchendX = 0;
-  const gestureZone = swipeBoxObj;
-  gestureZone.addEventListener('touchstart', function (event: any) {
-    touchstartX = event.changedTouches[0].screenX;
-  }, false);
-  gestureZone.addEventListener('touchend', function (event: any) {
-    touchendX = event.changedTouches[0].screenX;
-  }, false);
-}
 
+  public fnSwipeList() {
+    const swipeBoxes = document.querySelectorAll('.swipe-box');
+    swipeBoxes.forEach(swipeBox => {
+      const scroller = swipeBox.querySelector('.swipe-box__scroller');
+      if (scroller) {
+        scroller.scrollLeft += scroller.scrollWidth / 3;
+      }
+      this.fnDetectSwipe(swipeBox);
+    });
+  }
+
+  public fnDetectSwipe(swipeBoxObj: any) {
+    let touchstartX = 0;
+    let touchendX = 0;
+    const gestureZone = swipeBoxObj;
+    gestureZone.addEventListener('touchstart', function (event: any) {
+      touchstartX = event.changedTouches[0].screenX;
+    }, false);
+    gestureZone.addEventListener('touchend', (event: any) => {
+      touchendX = event.changedTouches[0].screenX;
+    }, false);
+  }
+
+}

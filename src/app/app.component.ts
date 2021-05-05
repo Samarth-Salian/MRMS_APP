919239ee86f1755a1d2dd65169b947bfe3f9a12b
bala453a1 <amalakanti.balakrishna@i-exceed.com>
2021-05-05 15:32:41 +0530

    profile page new screen design have been implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { NgxSpinnerService } from "ngx-spinner";
+import firebase from 'firebase/app';
+import { environment } from 'src/environments/environment';
 
 declare let window: any;
 @Component({
@@ -241,5 +243,60 @@ export class AppComponent {
   imageOnError(event: any) {
     this.showProfileImageIcon = true;
     this.showProfileImage = false;
+  }
+  signOut(): void {
+    this.showWelcomeMessage = false;
+    this.loginCredentials = '';
+    this.deleteLoginTable();
+  }
+  //Delete scenario
+  deleteLoginTable() {
+    if (window.cordova && window.cordova.platformId !== 'browser') {
+      window.plugins.googleplus.logout(
+        (obj: any) => {
+          this.clearTableContents();
+        },
+        (msg: any) => {
+          window.plugins.googleplus.trySilentLogin(
+            {
+              'webClientId': environment.WEB_APPLICATION_CLIENT_ID
+            },
+            (success: any) => {
+              window.plugins.googleplus.logout(
+                (data: any) => {
+                  this.clearTableContents();
+                }
+              );
+            },
+            (err: any) => {
+              console.log(err);
+            }
+          );
+        }
+      );
+    } else if (!window.cordova || window.cordova.platformId === 'browser') {
+      this.spinnerObj.show();
+      if (!firebase.apps.length) {
+        firebase.initializeApp(environment.firebaseConfig);
+      } else {
+          firebase.app();
+        }
+      firebase.auth().signOut()
+        .then(() => {
+          this.loginStorage.removeItem(this.tableName);
+          this.zone.run(() => { this.router.navigateByUrl('/signin', { state: { data: 'SignOut' } }); });
+        }, function (error) {
+            console.log('Signout Failed');
+        });
+    }
+  }
+  clearTableContents() {
+    this.db.transaction((tx: any) => {
+      tx.executeSql('DELETE  FROM ' + this.tableName);
+    }, function (error: any) {
+      console.log('Transaction ERROR: ' + error.message);
+    }, () => {
+      this.zone.run(() => { this.router.navigateByUrl('/signin', { state: { data: 'SignOut' } }); });
+    });
   }
 }
